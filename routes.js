51bd8a4bdbb9b54e7b7cbc6c93f4da5cd669d981
Gadareth Higgs <gadareth.h@gmail.com>
2016-05-02 20:11:58 -0400

    Remove Meteor sample code from about.html and add route to routes.js

--- routes.js
+++ routes.js
@@ -11,7 +11,17 @@ Router.route('/school', function () {
   this.render('school');
 });
 
+Router.route('/login', function () {
+  this.render('login');
+});
 
+Router.route('/school', function () {
+  this.render('school');
+});
+
+Router.route('/school', function () {
+  this.render('school');
+});
 // Router.route('/register', function () {
 //   this.render('register');
 //   this.layout('logRegHeader');
