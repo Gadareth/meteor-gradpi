5a652d8d5d9f9d93148c0c46f6c258cdccbca354
Gadareth Higgs <gadareth.h@gmail.com>
2016-05-07 14:23:54 -0400

    Fix form and link to profile

--- imports/collections.js
+++ imports/collections.js
@@ -22,12 +22,12 @@ if (Meteor.isServer) {
 	// This code runs only on server
 	// Only publish apprentices that are public or belong to the current user
 	Meteor.publish('apprentices', function() {
-	return Apprentices.find({"_id": this.userId});
-	})
+	return Apprentices.find({"_id": 1});
+	});
 
 	Meteor.methods({
 	'add_apprentice'(profile){
-		Apprentices.update({ "_id": Meteor.userId()  }, { "profile": profile, createdAt: new Date() }, { upsert: true });
+		Apprentices.update({ "_id": 1  }, { "profile": profile, createdAt: new Date() }, { upsert: true });
 			}
 		});
 }
--- server/main.js
+++ server/main.js
@@ -1,9 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
-
 Meteor.startup(() => {
   // code to run on server at startup
 });
 
-import './imports/collections.js';
+import '../imports/collections.js';
