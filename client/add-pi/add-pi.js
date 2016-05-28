09857c3f0de77d07d4b30898818b90e377197ab9
Gadareth Higgs <gadareth.h@gmail.com>
2016-05-28 10:08:55 -0400

    Format with commas and quotation marks

--- client/add-pi/add-pi.js
+++ client/add-pi/add-pi.js
@@ -13,7 +13,7 @@ Template.addpi.events({
 			Router.go('/rate-pi/'+result);
 		}
 	});
-	}
+	},
 	'change .myFileInput': function(event, template) {
       FS.Utility.eachFile(event, function(file) {
         Images.insert(file, function (err, fileObj) {
@@ -23,7 +23,7 @@ Template.addpi.events({
              // handle success depending what you need to do
             var userId = Meteor.userId();
             var imagesURL = {
-              “profile.image”: “/cfs/files/images/“ + fileObj._id
+              "profile.image": "/cfs/files/images/" + fileObj._id
             };
             Meteor.users.update(userId, {$set: imagesURL});
           }
