63b82bc253f34ad5252ea34c5e037324c8cec8d5
Gadareth Higgs <gadareth.h@gmail.com>
2016-07-24 14:32:07 -0400

    Format file spacing

--- client/add-pi/add-pi.js
+++ client/add-pi/add-pi.js
@@ -21,21 +21,20 @@ Template.addpi.events({
 
 	'click #piSubmit': function(event,template) {
 		event.preventDefault();	
-     	var name = template.find("#name").value;
-     	var school = template.find("#school").value;
-      	var dept = template.find("#dept").value;
-      	var image = imagesURL;
+   	var name = template.find("#name").value;
+   	var school = template.find("#school").value;
+    var dept = template.find("#dept").value;
+    var image = imagesURL;
       	
-	Meteor.call('add_advisor',name,school,dept,image, function(error,result){
-		// console.info("Added PI");
-		console.log(result);
-		if(error){
-			console.log(error);
-		} else {
-			console.log(imagesURL);
-			Router.go('/rate-pi/'+result);
-		}
-	});
-
+  	Meteor.call('add_advisor',name,school,dept,image, function(error,result){
+  		// console.info("Added PI");
+  		console.log(result);
+  		if(error){
+  			console.log(error);
+  		} else {
+  			console.log(imagesURL);
+  			Router.go('/rate-pi/'+result);
+  		}
+  	});
   }
 });
