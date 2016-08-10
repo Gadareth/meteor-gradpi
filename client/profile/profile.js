d10028de7c43c0d53d5e82bd93b3ee210fd04f6e
Gang Fan <gang.fan@uconn.edu>
2016-08-10 12:03:35 -0400

    profile showing ratings

--- client/profile/profile.js
+++ client/profile/profile.js
@@ -4,19 +4,19 @@ import { Advisors } from '../../imports/collections.js';
 
 Template.profile.onCreated(function profileOnCreated() {
 	Meteor.subscribe('advisors');
-	console.log(Router.current().params);
-	console.log(Advisors.find().fetch());
-	console.log(Advisors.findOne({_id: Router.current().params.id}));
+	// console.log(Router.current().params);
+	// console.log(Advisors.find().fetch());
+	// console.log(Advisors.findOne({_id: Router.current().params.id}));
 	
 });
 
 Template.profile.onRendered(function profileOnRendered(){
-	console.log(Advisors.findOne({_id: Router.current().params.id}));
-	ratings();
+	//console.log(Advisors.findOne({_id: Router.current().params.id}));
+	//ratings();
 });
-	var ratings = function(){
+/*	var ratings = function(){
 		console.log(Advisors.findOne({_id: Router.current().params.id}));
-		let advisor = Advisors.findOne({_id: Router.current().params.id}).fetch()[0];
+		let advisor = Advisors.findOne({_id: Router.current().params.id});
 		let overall_ratings = {};
 
 		//Compute average stature rating
@@ -93,6 +93,8 @@ Template.profile.onRendered(function profileOnRendered(){
 			t:t_overall.toFixed(0), 
 		};
 
+		console.log(overall_ratings);
+
 		for(property in overall_ratings){
 			switch(overall_ratings.property){
 				case 1:
@@ -112,7 +114,7 @@ Template.profile.onRendered(function profileOnRendered(){
 					break;
 			}
 		}
-	}
+	}*/
 
 
 
@@ -129,9 +131,103 @@ Template.profile.events({
 
 Template.profile.helpers({
 	thisAdvisor: function (){
-		console.log(Advisors.find().count());
+		//console.log(Advisors.find().count());
 		let returnVar = Advisors.findOne({_id: Router.current().params.id});
+		// console.log(returnVar);
+		// console.log(overall_ratings);
 		return returnVar;
+	},
+
+	ratings: function(){
+		let advisor = Advisors.findOne({_id: Router.current().params.id});
+		let overall_ratings = {};
+
+		//Compute average stature rating
+		let s_overall = advisor.stature;
+		let sum = 0;
+		for(let i = 0; i < s_overall.length; i++){
+			sum += s_overall[i];
+		}
+		if(s_overall.length > 0) {
+			s_overall = sum/s_overall.length;	
+		}
+		else {
+			s_overall = 0;
+		}
+		sum = 0;
+
+		//Compute average mentorship rating
+		let m_overall = advisor.mentorship;
+		for(let i = 0; i < m_overall.length; i++){
+			sum += m_overall[i];
+		}
+		if(m_overall.length > 0) {
+			m_overall = sum/m_overall.length;	
+		}
+		else {
+			m_overall = 0;
+		}
+		sum = 0;
+
+		//Compute average autonomy rating
+		let a_overall = advisor.autonomy;
+		for(let i = 0; i < a_overall.length; i++){
+			sum += a_overall[i];
+		}
+		if(a_overall.length > 0) {
+			a_overall = sum/a_overall.length;	
+		}
+		else {
+			a_overall = 0;
+		}
+		sum = 0;
+
+		//Compute average resources rating
+		let r_overall = advisor.resources;
+		for(let i = 0; i < r_overall.length; i++){
+			sum += r_overall[i];
+		}
+		if(r_overall.length > 0) {
+			r_overall = sum/r_overall.length;	
+		}
+		else {
+			r_overall = 0;
+		}
+		sum = 0;
+
+		//Compute average resources rating
+		let t_overall = advisor.tact;
+		for(let i = 0; i < t_overall.length; i++){
+			sum += t_overall[i];
+		}
+		if(t_overall.length > 0) {
+			t_overall = sum/t_overall.length;	
+		}
+		else {
+			t_overall = 0;
+		}
+		sum = 0;
+
+		overall_ratings = {
+			s:s_overall.toFixed(0),
+			m:m_overall.toFixed(0),
+			a:a_overall.toFixed(0),
+			r:r_overall.toFixed(0),
+			t:t_overall.toFixed(0), 
+		};
+
+		//console.log(overall_ratings);
+		//console.log(overall_ratings.s);
+
+		document.getElementById("sstar-"+overall_ratings.s.toString()).click();
+		document.getElementById("mstar-"+overall_ratings.m.toString()).click();
+		document.getElementById("astar-"+overall_ratings.a.toString()).click();
+		document.getElementById("rstar-"+overall_ratings.r.toString()).click();
+		document.getElementById("tstar-"+overall_ratings.t.toString()).click();
+		return overall_ratings; 	
+
+
+
 	}
 });
 
