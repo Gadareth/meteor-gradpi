eacb1dd52afa8f915927458da070b3a2ed3f46c0
ihorbrmk <paulbrmk@gmail.com>
2017-02-09 23:37:39 +0200

    stars output format to show only numeric values

--- client/shared/rating-stars/rating-stars.js
+++ client/shared/rating-stars/rating-stars.js
@@ -2,10 +2,14 @@ Template.ratingStars.onRendered(function(){
 	let $input = this.$('input.rating');
     //init stars plugin
     $input.removeClass('rating-loading').addClass('rating-loading').rating({
-    	readonly: !!Template.currentData().readonly
+    	readonly: !!Template.currentData().readonly,
+    	starCaptions(val) {
+    	    return val;
+    	}
     });
 	//do the stars live - reactive to any changes in PI's rating
 	this.autorun(()=>{
 		$input.rating('update', Template.currentData().value);
+
 	});
 })
