42b25b82826efdeb60ce52f7c657c5c4c9329c9f
ihorbrmk <paulbrmk@gmail.com>
2017-02-09 21:43:29 +0200

    changed smtp settings to gmail gradpi.app@gmail.com

--- server/email.js
+++ server/email.js
@@ -1,3 +1,4 @@
 Meteor.startup(function(){
-	process.env.MAIL_URL = 'smtp://postmaster%40mg.gradpi.com:e4fae6de27f1de001eb5569eb0665c87@smtp.mailgun.org:587';
+	// process.env.MAIL_URL = 'smtp://postmaster%40mg.gradpi.com:e4fae6de27f1de001eb5569eb0665c87@smtp.mailgun.org:587';
+	process.env.MAIL_URL = 'smtp://gradpi.app%40gmail.com:GradPIdev2@smtp.gmail.com:587';
 });
