69c9daaef216aedc579931a10f534deb7bb72560
ihorbrmk <paulbrmk@gmail.com>
2017-02-26 01:09:31 +0200

    fix facebook like button

---
+++ client/layouts/mainLayout/mainLayout.js
@@ -0,0 +1,18 @@
+Template.mainLayout.onRendered(function(){
+	window.fbAsyncInit = function() {
+	  FB.init({
+	    appId      : '856988154403595',
+	    xfbml      : true,
+	    version    : 'v2.8'
+	  });
+	  FB.AppEvents.logPageView();
+	};
+
+	(function(d, s, id){
+	   var js, fjs = d.getElementsByTagName(s)[0];
+	   if (d.getElementById(id)) {return;}
+	   js = d.createElement(s); js.id = id;
+	   js.src = "//connect.facebook.net/en_US/sdk.js";
+	   fjs.parentNode.insertBefore(js, fjs);
+	 }(document, 'script', 'facebook-jssdk'));
+});
